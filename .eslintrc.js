cf3dacd1b36e6e1205a8623fe5ee5aa52e00da5b
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2016-12-16 19:58:24 -0500

    Add initial skeleton for Clean Code workshop.

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ]
+};
---
+++ app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Good = require('good');
+
+require('./config/config')["dotEnv"];
+
+const server = new Hapi.Server();
+server.connection({ 
+    port: process.env.PORT || 3000 
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                    response: '*',
+                    log: '*'
+                }]
+            }, {
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+}, (err) => {
+
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
