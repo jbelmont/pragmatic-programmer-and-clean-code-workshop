84dbaf25d564fe16b8c2683a42a1c3cc3bc01167
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-01-08 21:49:34 -0500

    Added test for dip.

--- solid/dip/Repository.test.js
+++ solid/dip/Repository.test.js
@@ -2,16 +2,33 @@ const test = require('ava');
 const sinon = require('sinon');
 const {Repository} = require('./Repository');
 
-let insertStub, updateStub, retrieveStub, deleteStub;
-test.beforeEach(t => {
+let insertStub, updateStub, retrieveStub, deleteStub, sandbox;
+test.before(t => {
+  sandbox = sinon.sandbox.create();
   const Repo = new Repository();
-  insertStub = sinon.stub(Repo, 'insert');
-  updateStub = sinon.stub(Repo, 'update');
-  retrieveStub = sinon.stub(Repo, 'retrieveDocument');
-  deleteStub = sinon.stub(Repo, 'delete');
+  insertStub = sandbox.stub(Repo, 'insert');
+  insertStub.withArgs().throws({
+    'err': {
+      'message': '400'
+    },
+    'message': 'Missing name/document.'
+  });
+  updateStub = sandbox.stub(Repo, 'update');
+  retrieveStub = sandbox.stub(Repo, 'retrieveDocument');
+  deleteStub = sandbox.stub(Repo, 'delete');
   t.pass(true);
 });
 
-test('TestRepository should return methods', assert => {
-  assert.pass(true);
+test.after('cleanup', t => {
+  sandbox.restore();
+  t.pass(true);
+});
+
+test('test insert method in Repository Pattern throws expection when given improper arguments', assert => {
+  try {
+    insertStub();
+  } catch (e) {
+    sinon.assert.threw(insertStub);
+    assert.pass(true);
+  }
 });
