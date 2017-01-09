a804ecd1e5920ef9b54af53bba27a0421679b037
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-01-09 18:11:20 -0500

    Added more unit tests for dip.

--- solid/dip/Repository.test.js
+++ solid/dip/Repository.test.js
@@ -2,19 +2,44 @@ const test = require('ava');
 const sinon = require('sinon');
 const {Repository} = require('./Repository');
 
-let insertStub, updateStub, retrieveStub, deleteStub, sandbox;
+let insertStub, insertDoc, insertResult, updateStub, retrieveStub, retrieveDoc, retrieveResult, deleteStub, sandbox;
 test.before(t => {
   sandbox = sinon.sandbox.create();
   const Repo = new Repository();
   insertStub = sandbox.stub(Repo, 'insert');
-  insertStub.withArgs().throws({
+  insertStub.withArgs().returns(Promise.reject({
     'err': {
-      'message': '400'
+      'statusCode': '400'
     },
     'message': 'Missing name/document.'
-  });
+  }));
+  insertDoc = {
+    dbName: 'pragmaticprogrammer',
+    name: 'example1',
+    body: {
+      hero1: 'hero1',
+      hero2: 'hero2'
+    }
+  };
+  insertResult = {
+    _id: 'example1',
+    _rev: '5-933532f44c219640a9cd38a813316ac0',
+    hero1: 'hero1',
+    hero2: 'hero2'
+  };
+  insertStub.withArgs(insertDoc.dbName, insertDoc.name, insertDoc.body).returns(Promise.resolve(insertResult));
   updateStub = sandbox.stub(Repo, 'update');
   retrieveStub = sandbox.stub(Repo, 'retrieveDocument');
+  retrieveDoc = {
+    dbName: 'pragmaticprogrammer',
+    name: 'example1'
+  };
+  retrieveResult = {
+    _id: 'example1',
+    _rev: '7-04aeae92f29821ac73c52b5569f5064e',
+    hero1: 'hero1',
+    hero2: 'hero2'
+  };
   deleteStub = sandbox.stub(Repo, 'delete');
   t.pass(true);
 });
@@ -25,10 +50,32 @@ test.after('cleanup', t => {
 });
 
 test('test insert method in Repository Pattern throws expection when given improper arguments', assert => {
+  insertStub()
+    .then(result => result)
+    .catch(error => {
+      const actual = error['err']['statusCode'];
+      const expected = '400';
+      assert.is(actual, expected, `should return status code of ${expected}`);
+    });
+});
+
+test('test insert method in Repository Pattern returns an object when given proper arguments', assert => {
+  insertStub(insertDoc.dbName, insertDoc.name, insertDoc.body).then(actual => {
+    const expected = insertResult;
+    assert.is(actual, expected, `should return ${expected}`);
+  });
+});
+
+test('test retrieveDocument method in Repository Pattern returns object when given proper arguments', assert => {
   try {
-    insertStub();
-  } catch (e) {
-    sinon.assert.threw(insertStub);
-    assert.pass(true);
+    const {Repository} = require('./Repository');
+    const repo = new Repository();
+    repo.retrieveDocument(retrieveDoc).then(result => {
+      const {hero1} = result;
+      const expected = 'hero1';
+      assert.is(hero1, expected, `should return ${expected}`);
+    });
+  } catch (error) {
+    console.log(error);
   }
 });
