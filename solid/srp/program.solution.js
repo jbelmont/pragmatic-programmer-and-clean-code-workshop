a0e2202b2cb56b1bfc8758c4f5038d181d45a91b
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-01-19 10:48:38 -0500

    Remove function from program.solution.js

--- solid/srp/program.solution.js
+++ solid/srp/program.solution.js
@@ -3,30 +3,6 @@
 const fs = require('fs');
 const {join} = require('path');
 
-const readSoldiersFormatSoldiersAndWriteSoldiers = () => {
-  return fs.readFile(join(__dirname, '../../data/soldiers.csv'), (err, data) => {
-    if (err) {
-      throw err;
-    }
-    const soldiers = data.toString()
-      .trim()
-      .split('\n')
-      .map(soldier => soldier.split(','))
-      .map((field) => {
-        return {
-          name: field[0],
-          rank: field[1],
-          branch: field[2]
-        };
-      });
-    return fs.writeFile(join(__dirname, 'soldiers.json'), JSON.stringify(soldiers), (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-  });
-};
-
 const readSoldiers = (cb) => {
   return fs.readFile(join(__dirname, '../../data/soldiers.csv'), (err, data) => {
     if (err) {
@@ -64,7 +40,6 @@ const removeSoldiers = (file, cb) => {
 };
 
 module.exports = {
-  readSoldiersFormatSoldiersAndWriteSoldiers,
   readSoldiers,
   formatSoldiers,
   writeSoldiers,
